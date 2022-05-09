199520061e470e42ac27fd70de256d85d4b13c77
Ubuntu <azureuser@albert.vvyk3vk0fq5uvgjaumtfaz5z2b.hx.internal.cloudapp.net>
2022-05-09 08:45:06 +0000

    Display the effect of aligned_double4 in dynamic allocating.

--- aligned_heap_vectorization.cpp
+++ aligned_heap_vectorization.cpp
@@ -24,8 +24,10 @@ public:
   void operator delete (void* ptr) {
     ::operator delete(*(reinterpret_cast<void**>(ptr) - 1));
   }
-
+  
+  int x;
   aligned_double4 data;
+  int y, z;
 };
 
 Vector4d operator+ (const Vector4d& v1, const Vector4d& v2) {
@@ -46,8 +48,10 @@ int main() {
   Vector4d* input1 = new Vector4d{1, 1, 1, 1};
   Vector4d* input2 = new Vector4d{1, 2, 3, 4};
 
+  std::cout << "address of input1.x: " << &input1->x << std::endl;
   std::cout << "address of input1: " << input1->data << std::endl;
-  std::cout << "address of input2: " << input2->data << std::endl;
+  std::cout << "address of input1.y: " << &input1->y << std::endl;
+  std::cout << "address of input1.z: " << &input1->z << std::endl;
 
   Vector4d result = *input1 + *input2;
 
